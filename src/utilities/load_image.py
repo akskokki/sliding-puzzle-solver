002aaf35ea10732fc8420a59125e94ad7a7ad658
akskokki <akseli.kokki@gmail.com>
2023-04-24 03:37:07 +0300

    Fix image loading

--- src/utilities/load_image.py
+++ src/utilities/load_image.py
@@ -6,5 +6,5 @@ directory = os.path.dirname(__file__)
 
 def load_image(file):
     return pygame.image.load(
-        os.path.join(directory, 'assets', file)
+        os.path.join(directory, '../assets', file)
     )
